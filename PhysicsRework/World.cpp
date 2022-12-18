ca0b472b5168235ca826c4ab30bc4be1225d8a3b
Hallam Rear <hallamrear@gmail.com>
2022-12-18 00:19:33 +0000

    Added a static vector that I can use to draw debug points to the screen. Reused one of the blocks as a constant floor.

--- PhysicsRework/World.cpp
+++ PhysicsRework/World.cpp
@@ -2,11 +2,14 @@
 #include "SDL.h"
 #include <SDL_rect.h>
 
+std::vector<SDL_Point> World::DebugPointsToRenderThisFrame = std::vector<SDL_Point>();
+
 void World::Setup()
 {
 	m_Bodies = std::vector<Body*>();
 
-	m_Bodies.push_back(new Body(400, 200, 64.0f, 100.0f));
+	m_Bodies.push_back(new Body(400, 780, 400, FLT_MAX)); //Floor
+
 	m_Bodies.push_back(new Body(400, 400, 64.0f, 100.0f));
 }
 
@@ -33,6 +36,12 @@ void World::Render(SDL_Renderer* renderer)
 		SDL_RenderDrawLine(renderer, (int)itr->TR.X, (int)itr->TR.Y, (int)itr->BR.X, (int)itr->BR.Y);
 		SDL_RenderDrawLine(renderer, (int)itr->BR.X, (int)itr->BR.Y, (int)itr->BL.X, (int)itr->BL.Y);
 		SDL_RenderDrawLine(renderer, (int)itr->BL.X, (int)itr->BL.Y, (int)itr->TL.X, (int)itr->TL.Y);
+	}
+
+	if (DebugPointsToRenderThisFrame.size() > 0)
+	{
+		SDL_RenderDrawPoints(renderer, DebugPointsToRenderThisFrame.data(), DebugPointsToRenderThisFrame.size());
+		DebugPointsToRenderThisFrame.clear();
 	}
 
 	SDL_RenderPresent(renderer);
--- PhysicsRework/World.h
+++ PhysicsRework/World.h
@@ -6,6 +6,8 @@ struct SDL_Renderer;
 
 struct World
 {
+	static std::vector<SDL_Point> DebugPointsToRenderThisFrame;
+
 	std::vector<Body*> m_Bodies;
 
 	void Setup();
